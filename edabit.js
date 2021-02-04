584d313d01645b6472dc7a44743f820951dd9379
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-02-04 10:59:28 +0100

    solucion peso

--- edabit.js
+++ edabit.js
@@ -12,37 +12,20 @@ function orderWeight(strng) {
          }
     
     );
-   //return arrayNum.sort((a, b) => a[0] - b[0]).map(el => el[1]); respuesta buena sin los strigs
-    
-    let flato = arrayNum.flat();
-    let objeto = Object.fromEntries(arrayNum);
-
-  /*  let sorteado = arrayNum.sort((a, b) => {
-        let indexito = flato.indexOf(a[0]);
-       
-        let vacio = [];
-        if (flato.indexOf(a[0], indexito + 1) !== -1) {
-            vacio.push(a[1])
-            return a[1] - b[1];
-       
+    let sorteado = arrayNum.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1].localeCompare(b[1]);
         } else {
-           return a[0] -b[0]
+            return a[0] - b[0];
         }
-
     });
+  
 
-    return sorteado;
-*/
-    let vacio = [];
-    for (let i = 0; i < arrayNum.length; i++){
-        
-        if (flato.indexOf(arrayNum[i][0], flato.indexOf(arrayNum[i][0]) + 1) !== -1) {
-            vacio.push(arrayNum[i][0])
-        }
-    }
-    return vacio;
+    return sorteado.map(el => el[1]).join(' ');
+    
+
 };
-
+//"11 11 2000 10003 22 123 1234000 44444444 9999")
 
 
 console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
