1cc933b5e0889d28db7340c58551cb840dca0cad
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-02-08 14:40:51 +0100

    solucion inteligente code wars

--- encode.js
+++ encode.js
@@ -1,39 +1,12 @@
 
-let vocales = {
-    'a' :1,
-    'e': 2,
-    'i': 3,
-    'o': 4,
-    'u': 5
-}
-
-
-function encode(string) {
-
-    for (let i = 0; i < string.length; i++){
-        if (vocales.hasOwnProperty(string[i])) {
-        
-         string = string.replace(string[i], vocales[string[i]]);
-        
-        } 
-    }
-    return string;
-
+// smart solution code wars:
+function encode(string){
+    return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
   }
   
-function decode(string) {
-
-    
-    for (let j = 0; j < string.length; j++){
-        for (let key in vocales) {
-            if (vocales[key] == string[j]) {
-                string = string.replace(string[j], key)
-                continue;
-            }
-        }
-    }
-    return string;
+  //turn numbers back into vowels
+  function decode(string){
+    return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
   }
-
 console.log(encode('hello'));
 console.log(decode('h2ll4'));
