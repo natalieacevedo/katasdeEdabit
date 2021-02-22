81eb1d68bab39d6f0337be7aa6f83d8f409af04d
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-02-22 11:08:58 +0100

    solucion fidel

--- mas.js
+++ mas.js
@@ -1,33 +1,31 @@
 function highestRank(arr) {
-    let objeto = {};
+    let cuentas = new Map();
+    let n = 0;
+    let max = 0;
+    let candidates = [];
     
-    for (let i = 0; i < arr.length; i++){
-        let cuenta = 0;
-        let nuevo = arr;
-       // console.log(nuevo);
-         
-        while (nuevo.indexOf(arr[i]) !== -1) {
-            let indexo = nuevo.indexOf(arr[i]);
-            cuenta++;
-            nuevo = nuevo.slice(indexo + 1);
-            objeto[arr[i]] = cuenta;
-
+    for (let num of arr) {
+        n = cuentas.get(num);
+        if (n === undefined) {
+            n = 0;
         }
-       
-    };
-    
-    return arr.reduce((acc, el) => {
-        if (objeto[acc] > objeto[el]) {
-            return acc;
-        } else if (objeto[acc] == objeto[el]) {
-            return Math.max(acc, el)
-        } else {
-            return el
+        n++;
+        cuentas.set(num, n);
+        if (n > max) {
+            max = n;            
         }
-    });
+    }
 
-    
+    for (let [n, count] of cuentas.entries()) {
+        if (count === max) {
+            candidates.push(n);
+        }
+    }
+
+    console.log(candidates);
+
+    return Math.max(...candidates);
 };
 
-var arr = [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10];
+var arr = [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10, 10];
 console.log(highestRank(arr));
