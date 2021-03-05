36449fa70c11478bff09cefe8be6f3a20d19d13b
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-05 10:23:42 +0100

    slolucion smarter de Fidel

--- tenMinuteWalk.js
+++ tenMinuteWalk.js
@@ -1,33 +1,15 @@
 function isValidWalk(walk) {
-    
-    if (walk.length !== 10) {
-        
-        return false
+    if(walk.length !== 10){
+      return false
     } else {
-        let nadira = {};
-
-        walk.forEach(element => {
-            nadira[element] = 0;
-
-        });
-    
-    
-    
-        for (let i = 0; i < walk.length; i++) {
-        
-            if (nadira.hasOwnProperty(walk[i])) {
-                nadira[walk[i]] += 1;
-            }
-
-        }
-        let directions = [['n', 's'], ['e', 'w']];
-
-        for (let j = 0; j < directions.length; j++) {
-            if (nadira[directions[j][0]] !== nadira[directions[j][1]]) {
-                return false
-            }
-        }
-        return true;
+      let nadira = {};
+      walk.forEach(element => {
+       let count = nadira[element] || 0;
+       count++;
+       nadira[element] = count;
+      });
+      
+      return nadira['n'] == nadira['s'] && nadira['e'] == nadira['w'];
     }
 };
 
