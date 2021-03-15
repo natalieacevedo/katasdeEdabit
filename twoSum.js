b7d7b24d5c920dd2f83381fd5fa6e2791b4aa34b
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-15 23:45:15 +0100

    segunda solucion con for Each

--- twoSum.js
+++ twoSum.js
@@ -1,14 +1,28 @@
-function twoSum(numbers, target) {
-  
-  
-    for (let i = 0; i < numbers.length; i++){
-        for (let j = 1; j < numbers.length; j++){
-            if (numbers[i] + numbers[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-  
-};
 
-console.log(twoSum([2,2,3], 4))
+  
+    function twoSum(numbers, target) {
+      
+      let empty = {}
+
+      numbers.forEach((element, index) => {
+        
+        empty[element] = index;
+
+      });
+      
+      
+      for (let i = 0; i < numbers.length; i++){
+
+        let diferencia = target - numbers[i];
+
+        if (empty[diferencia] !== undefined) {
+          return [i, empty[diferencia]];
+        }
+
+      }
+};
+   
+  
+
+
+console.log(twoSum([1234, 5678, 9012], 14690));
