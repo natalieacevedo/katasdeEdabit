e18dd64b3431f2f5e9b7afb3e11a8adca55d83e0
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-23 18:24:11 +0100

    Solution with every

--- CodingMeetup.js
+++ CodingMeetup.js
@@ -2,18 +2,24 @@ function allContinents(list) {
     
     let continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
-    return list.reduce((acc, el) => {
-      
-        if (continents.indexOf(el.continent) !== -1 && acc.indexOf(el.continent) === -1) {
-            return acc.concat(el.continent);
-        };
-        return acc;
-    }, []).length === continents.length;
+   return continents.every(el => {
+        
+        for (let i = 0; i < list.length; i++){
+            if (list[i].continent === el) {
+                return el;
+            } else {
+                continue;
+            }
+        }
+   })
+   
+   
+    
 };
 
 let list1 = [
-    { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
-    { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+    { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Asia', age: 25, language: 'JavaScript' },
+    { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Europe', age: 37, language: 'C' },
     { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Africa', age: 39, language: 'Ruby' },
     { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
     { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
